fb30ab49977e207a229e6dfb4ec05ebc3fb14de9
Tatiana <in_sdo@mail.ru>
2023-09-21 09:56:41 +0300

    add exception msg

--- src/main/java/com/example/testbooksandauthor/controller/ExceptionController.java
+++ src/main/java/com/example/testbooksandauthor/controller/ExceptionController.java
@@ -2,6 +2,7 @@ package com.example.testbooksandauthor.controller;
 
 import com.example.testbooksandauthor.domain.ErrorResponse;
 import com.example.testbooksandauthor.exception.AuthorNotFoundException;
+import com.example.testbooksandauthor.exception.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,5 +15,14 @@ public class ExceptionController {
     public ErrorResponse notFound(AuthorNotFoundException e) {
         return new ErrorResponse(e.getMessage());
     }
-
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(BookNotFoundException.class)
+    public ErrorResponse notFound(BookNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(RuntimeException.class)
+    public ErrorResponse error(RuntimeException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
 }
