cc430c50f55c276d52740c1ed3ef70d662a8d2ca
Tatiana <in_sdo@mail.ru>
2023-09-27 12:31:55 +0300

    delete

--- src/main/java/com/example/testbooksandauthor/repository/BookAuthorRelationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.testbooksandauthor.repository;
-
-import com.example.testbooksandauthor.model.BookAuthorRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookAuthorRelationRepository extends JpaRepository<BookAuthorRelation, Integer> {
-}
