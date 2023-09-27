e4189773b476ff4f45bdd7e27bdf1f87675930ca
Tatiana <in_sdo@mail.ru>
2023-09-27 13:17:33 +0300

    delete

--- src/main/java/com/example/testbooksandauthor/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.testbooksandauthor.exception;
-
-public class AuthorNotFoundException extends RuntimeException{
-    public AuthorNotFoundException(String msg){
-        super(msg);
-    }
-}
--- src/main/java/com/example/testbooksandauthor/exception/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.testbooksandauthor.exception;
-
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(final String msg) {
-        super(msg);
-    }
-}
